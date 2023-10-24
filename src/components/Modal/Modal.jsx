66dff080211b5f48a6bbfdc4b6d04a24ee6e94ea
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-24 11:05:49 +0300

    change11

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -35,7 +35,7 @@ class Modal extends Component {
 }
 
 export default Modal;*/
-
+/*
 import React, { useEffect } from 'react';
 import css from './Modal.module.css';
 
@@ -69,4 +69,38 @@ function Modal({ largeImg, tag, closeModal }) {
   );
 }
 
+export default Modal;*/
+import React, { useEffect } from 'react';
+import css from './Modal.module.css';
+
+function Modal({ largeImg, tag, closeModal }) {
+  const closeModalEsc = (e) => {
+    if (e.code === 'Escape') {
+      closeModal();
+    }
+  };
+
+  const closeModalBackdrop = (e) => {
+    if (e.currentTarget === e.target) {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', closeModalEsc);
+
+    return () => {
+      window.removeEventListener('keydown', closeModalEsc);
+    };
+  }, [closeModalEsc]); // Додайте closeModalEsc до залежностей
+  // При цьому, якщо closeModalEsc не змінюється, можна використовувати пустий масив залежностей ([]), якщо він не залежить від інших значень або стану.
+
+  return (
+    <div onClick={closeModalBackdrop} className={css.overlay}>
+      <div className={css.modal}>
+        <img src={largeImg} alt={tag} />
+      </div>
+    </div>
+  );
+}
 export default Modal;
