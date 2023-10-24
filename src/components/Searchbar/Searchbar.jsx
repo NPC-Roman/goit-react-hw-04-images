00ddcda8379a80e8eb07e09cdc63e7d18bd84611
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-24 10:34:57 +0300

    change07

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import css from './Searchbar.module.css';
 import { BiSearch } from 'react-icons/bi';
 import { toast } from 'react-toastify';
@@ -10,7 +10,6 @@ class Searchbar extends Component {
 
   handleImageNameChange = evt => {
     this.setState({ imageName: evt.currentTarget.value.toLowerCase() });
-    // console.log(evt.currentTarget.value.toLowerCase());
   };
 
   handleFormSubmit = evt => {
@@ -45,6 +44,50 @@ class Searchbar extends Component {
       </header>
     );
   }
+}
+export default Searchbar;*/
+import React, { useState } from 'react';
+import css from './Searchbar.module.css';
+import { BiSearch } from 'react-icons/bi';
+import { toast } from 'react-toastify';
+
+function Searchbar({ onSubmit }) {
+  const [imageName, setImageName] = useState('');
+
+  const handleImageNameChange = (evt) => {
+    setImageName(evt.currentTarget.value.toLowerCase());
+  };
+
+  const handleFormSubmit = (evt) => {
+    evt.preventDefault();
+    if (imageName.trim() === '') {
+      toast.error('Empty string');
+      return;
+    }
+    onSubmit(imageName);
+    setImageName('');
+  };
+
+  return (
+    <header className={css.searchbar}>
+      <form onSubmit={handleFormSubmit} className={css.searchForm}>
+        <button type="submit" className={css.searchFormButton}>
+          <BiSearch size="25px" />
+          <span className={css.searchFormButtonLabel}>Search</span>
+        </button>
+
+        <input
+          className={css.searchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={handleImageNameChange}
+          value={imageName}
+        />
+      </form>
+    </header>
+  );
 }
 
 export default Searchbar;
