bbb3d5ab38aa44f42da1001ab2b24257c9b4044a
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-24 10:22:20 +0300

    chanhe06

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import css from './Modal.module.css';
 
 class Modal extends Component {
@@ -32,6 +32,41 @@ class Modal extends Component {
       </div>
     );
   }
+}
+
+export default Modal;*/
+
+import React, { useEffect } from 'react';
+import css from './Modal.module.css';
+
+function Modal({ largeImg, tag, closeModal }) {
+  const closeModalEsc = (e) => {
+    if (e.code === 'Escape') {
+      closeModal();
+    }
+  };
+
+  const closeModalBackdrop = (e) => {
+    if (e.currentTarget === e.target) {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', closeModalEsc);
+
+    return () => {
+      window.removeEventListener('keydown', closeModalEsc);
+    };
+  }, []);
+
+  return (
+    <div onClick={closeModalBackdrop} className={css.overlay}>
+      <div className={css.modal}>
+        <img src={largeImg} alt={tag} />
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
