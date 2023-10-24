71029f7a44dd6f4e3388d1cbcf46dd4f10ee13a2
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-24 10:10:07 +0300

    change05

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,4 +1,4 @@
-import Modal from 'components/Modal';
+/*import Modal from 'components/Modal';
 import React, { Component } from 'react';
 import css from './ImageGalleryItem.module.css';
 
@@ -29,6 +29,34 @@ class ImageGalleryItem extends Component {
       </>
     );
   }
+}
+
+export default ImageGalleryItem;*/
+
+import React, { useState } from 'react';
+import Modal from 'components/Modal';
+import css from './ImageGalleryItem.module.css';
+
+function ImageGalleryItem({ webFormat, tag, largeFormat }) {
+  const [openModal, setOpenModal] = useState(false);
+
+  const toggleModal = () => {
+    setOpenModal(!openModal);
+  };
+
+  return (
+    <>
+      <img
+        className={css.imageGalleryItemImage}
+        src={webFormat}
+        alt={tag}
+        onClick={toggleModal}
+      />
+      {openModal && (
+        <Modal closeModal={toggleModal} largeImg={largeFormat} tag={tag} />
+      )}
+    </>
+  );
 }
 
 export default ImageGalleryItem;
