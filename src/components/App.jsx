f2af238744e328740da173d58a8ed3665c57f2a9
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-24 09:42:05 +0300

    change04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-
+/*import React, { Component } from 'react';
 import ImageGallery from './ImageGallery';
 import Searchbar from './Searchbar';
 import Button from './Button';
@@ -67,6 +66,68 @@ class App extends Component {
       </>
     );
   }
+}
+export default App;*/
+
+import React, { useState, useEffect } from 'react';
+
+import ImageGallery from './ImageGallery';
+import Searchbar from './Searchbar';
+import Button from './Button';
+import Loader from './Loader';
+import { getImages } from './api';
+
+function App() {
+  const [imageName, setImageName] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const perPage = 12;
+
+  const getImages = () => {
+    setLoading(true);
+    getImages(imageName, page, perPage)
+      .then((res) => {
+        setImages((prevImages) => [...prevImages, ...res.data.hits]);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    if (imageName !== '' && page === 1) {
+      setImages([]);
+      getImages();
+    }
+  }, [imageName, page]);
+
+  const loadMore = () => {
+    setPage(page + 1);
+  };
+
+  const handleChangeName = (newImageName) => {
+    setImageName(newImageName);
+    setPage(1);
+  };
+
+  return (
+    <>
+      <Searchbar onSubmit={handleChangeName} />
+
+      <ImageGallery images={images} />
+
+      {images.length === 0 && !loading && (
+        <h2 style={{ textAlign: 'center' }}>No images for showing</h2>
+      )}
+
+      {loading && <Loader />}
+
+      {images.length >= perPage && images.length % perPage === 0 && !loading && (
+        <Button onClick={loadMore} />
+      )}
+    </>
+  );
 }
 
 export default App;
